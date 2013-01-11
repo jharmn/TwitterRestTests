6787d5dcdc54e824713a79032efcb2aabe5ef3e9
Jason Harmon <jason@wheelspecs.com>
2013-01-10 23:24:09 -0600

    Initial checkin

---
+++ TwitterRestTests/RestTestSteps.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace TwitterRestTests
+{
+    [Binding]
+    public class RestTestSteps
+    {
+        private string url;
+        private string content;
+        private WebClient wc = new WebClient();
+        JObject response;
+        HttpStatusCode httpStatus;
+
+        [Given(@"I access the resource url ""(.*)""")]
+        public void GivenIAccessTheResourceUrl(string resourceUrl)
+        {
+            this.url = "https://api.twitter.com" + resourceUrl;
+        }
+
+        [When(@"I retrieve the results")]
+        public void WhenRetrieveTheResults()
+        {
+            try
+            {
+                this.content = wc.DownloadString(url);
+                this.httpStatus = HttpStatusCode.OK;
+            }
+            catch (WebException we)
+            {
+                this.httpStatus = ((HttpWebResponse)we.Response).StatusCode;
+            }
+            if (this.httpStatus.Equals(HttpStatusCode.OK))
+            {
+                Assert.IsNotNullOrEmpty(this.content);
+                this.response = (JObject)JToken.Parse(this.content);
+            }
+        }
+
+        [Then(@"the status code should be (.*)")]
+        public void ThenTheStatusCodeShouldBe(int statusCode)
+        {
+            Assert.AreEqual(statusCode, (int)this.httpStatus);
+        }
+
+        [Then(@"it should have the field ""(.*)"" containing the value ""(.*)""")]
+        public void ThenItShouldContainTheFieldContainingTheValue(string field, string value)
+        {
+            if (response != null)
+            {
+                JValue val = (JValue)this.response.GetValue(field);
+                string valStr = val.Value<string>().Trim();
+                Assert.IsNotNull(valStr);
+                Assert.AreEqual(valStr, value.Trim());
+            }
+        }
+
+        [Then(@"it should have the field ""(.*)"" containing the value (.*)")]
+        public void ThenItShouldContainTheFieldContainingTheValue(string field, int value)
+        {
+            if (response != null)
+            {
+                JValue val = (JValue)this.response.GetValue(field);
+
+                int valInt = val.Value<Int32>();
+                Assert.AreEqual(valInt, value);
+            }
+        }
+    }
+}
---
+++ TwitterRestTests/User.feature
@@ -0,0 +1,15 @@
+﻿Feature: Twitter user lookup
+
+@Positive @Get @User
+Scenario: Show twitter user
+	Given I access the resource url "/1/users/show.json?screen_name=jasonh_n_austin&include_entities=true"
+	When I retrieve the results
+	Then the status code should be 200
+	And it should have the field "name" containing the value "Jason Harmon"
+	And it should have the field "id" containing the value "57005215"
+
+@Negative @Get @User
+Scenario: Invalid twitter user
+	Given I access the resource url "/1/users/show.json?screen_name=jasonh_n_portland&include_entities=true"
+	When I retrieve the results
+	Then the status code should be 404
